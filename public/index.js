c2c9c2eafa065c83afcf6e793ccba99140d1673c
Md. Rabbinur Muktar <59724876+Rabbinur@users.noreply.github.com>
2025-01-20 17:36:31 +0600

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,225 @@
+import menu from "./images/menu.svg";
+
+import close from "./images/close.svg";
+
+import logo from "./images/learn.webp";
+
+import facebook from "./images/facebook.svg";
+
+import twitter from "./images/twitter.svg";
+
+import linkedin from "./images/linkedin.svg";
+
+import instagram from "./images/instagram.svg";
+
+import uparrow from "./images/up-arrow.svg";
+
+import downarrow from "./images/down-arrow.svg";
+
+import phone from "./images/phone.svg";
+
+import email from "./images/email.svg";
+
+import graphics from "./images/graphics.webp";
+
+import cad from "./images/auto-cad.webp";
+
+import office from "./images/office-application.webp";
+
+import digital from "./images/social-media.webp";
+
+import mechanic from "./images/mechanic.webp";
+
+import english from "./images/teacher.webp";
+
+import autocad from "./images/auto-cad.webp";
+
+import care from "./images/donation.webp";
+
+import dressmake from "./images/sewing.webp";
+
+import korean from "./images/korean.webp";
+
+import housewiring from "./images/broken-wire.webp";
+
+import ict from "./images/textbook.webp";
+
+import instractor1 from "./images/instractor1.webp";
+
+import instractor2 from "./images/instractor2.webp";
+
+import instractor3 from "./images/instractor3.webp";
+
+import instractor4 from "./images/instractor4.webp";
+
+import instractor5 from "./images/instractor5.webp";
+
+import instractor6 from "./images/instractor6.webp";
+
+import instractor7 from "./images/instractor7.webp";
+
+import instractor8 from "./images/instractor8.webp";
+
+import instractor9 from "./images/instractor9.webp";
+
+import instractor10 from "./images/instractor10.webp";
+
+import rating from "./images/rating.png";
+
+import student1 from "./images/student1.jpg";
+import student2 from "./images/student2.jpg";
+import student3 from "./images/student3.jpg";
+import student4 from "./images/student4.jpg";
+import student5 from "./images/student5.jpg";
+import student6 from "./images/student6.jpg";
+import student7 from "./images/student7.jpg";
+import student8 from "./images/student8.jpg";
+import student9 from "./images/student9.jpg";
+
+import courseweb from "./images/courseweb.webp";
+
+import lessonimg from "./images/chapter.png";
+
+import day from "./images/clock.png";
+
+import signal from "./images/signal.png";
+
+import middle from "./images/banner-title-image-1.webp";
+import carousel1 from "./images/hero_bg_6_1-6-1.jpg";
+import carousel2 from "./images/hero_bg_6_2-6-1.jpg";
+import carousel3 from "./images/hero_bg_6_3-6-1.jpg";
+import carousel4 from "./images/banner-title-image-1.webp";
+
+import carouselmobile1 from "./images/carousel1mobile.webp";
+import carouselmobile2 from "./images/carousel2mobile.webp";
+import carouselmobile3 from "./images/carousel3mobile.webp";
+import carouselmobile4 from "./images/carousel4mobile.webp";
+
+import course1 from "./images/course-1.webp";
+import course2 from "./images/course-2.webp";
+import course3 from "./images/course-3.webp";
+import course4 from "./images/course-4.webp";
+import course5 from "./images/course-5.webp";
+import course6 from "./images/course-6.webp";
+import course7 from "./images/course-7.webp";
+import course8 from "./images/course-8.webp";
+import course9 from "./images/course-9.webp";
+import course10 from "./images/course-10.webp";
+
+import shape from "./images/shape.svg";
+
+import shape2 from "./images/shape-2.svg";
+
+import shape3 from "./images/banner-3.png";
+
+import location from "./images/location.svg";
+
+import gallery_1 from "./images/Gallery1.jpg";
+
+import gallery_2 from "./images/gallery2.jpg";
+
+import gallery_3 from "./images/gallery3.jpg";
+
+import gallery_4 from "./images/Gallery4.jpg";
+
+import gallery_5 from "./images/gallery5.jpg";
+
+import gallery_6 from "./images/gallery6.jpg";
+
+import gallery_7 from "./images/gallery7.jpg";
+
+import nrb from "./images/nrbbank.png";
+
+import mtb from "./images/mtbl.webp";
+
+import bteb from "./images/bteb.png";
+
+import gov from "./images/gov.png";
+
+import about from "./images/payment.webp";
+import about_4 from "./images/about_4.webp";
+export {
+  carousel1,
+  carousel2,
+  carousel3,
+  carousel4,
+  carouselmobile1,
+  carouselmobile2,
+  carouselmobile3,
+  carouselmobile4,
+  about,
+  nrb,
+  mtb,
+  bteb,
+  gov,
+  email,
+  phone,
+  location,
+  uparrow,
+  downarrow,
+  mechanic,
+  menu,
+  close,
+  logo,
+  facebook,
+  twitter,
+  instagram,
+  linkedin,
+  care,
+  housewiring,
+  korean,
+  dressmake,
+  graphics,
+  cad,
+  office,
+  english,
+  digital,
+  ict,
+  middle,
+  instractor1,
+  instractor2,
+  instractor3,
+  instractor4,
+  instractor5,
+  instractor6,
+  instractor7,
+  instractor8,
+  instractor9,
+  instractor10,
+  course1,
+  course2,
+  course3,
+  course4,
+  course5,
+  course6,
+  course7,
+  course8,
+  course9,
+  course10,
+  rating,
+  student1,
+  student2,
+  student3,
+  student4,
+  student5,
+  student6,
+  student7,
+  student8,
+  student9,
+  courseweb,
+  lessonimg,
+  day,
+  signal,
+  autocad,
+  shape,
+  shape2,
+  shape3,
+  gallery_1,
+  gallery_2,
+  gallery_3,
+  gallery_4,
+  gallery_5,
+  gallery_6,
+  gallery_7,
+  about_4,
+};
